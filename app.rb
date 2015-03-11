06369cc213819fc14784581943df8896689d7ead
Divyum <divyum@Divyums-MacBook-Air.local>
2015-03-11 15:07:09 +0530

    issues fixed, all outputs are json

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ Thread.new do
 end
 
 get '/' do
-	'Connected'
+	res = {}
+	res['status'], res['message'] = 'true', 'Connected'
+	content_type :json
+	return res.to_json
 end
 
 get '/keys' do
@@ -26,46 +29,86 @@ get '/key/get' do
 	#endpoint to get key
 	key = server.get
 	return '404' if key == nil
-	key.to_s
+	content_type :json
+	return key
 end
 
 
 get '/key/free' do
 	#endpoint to get list of free keys
 	k = server.free
-	res = k.length>0 ? k.to_json + "</br> #{k.length}" : "No key generated"
+ 	if k.length>0
+ 		free={}
+ 		free['keys'], free['count'] = k, k.length
+		res = free.to_json
+	else
+		 res = "No key generated"
+	end
+	content_type :json
+	return res
 end
 
 
 get '/key/blocked' do
 	#endpoint to get list of blocked keys
 	k = server.blocked
-	res = k.length>0 ? k.to_json + "</br> #{k.length}" : "No key generated"
+	if k.length>0
+		blocked={}
+ 		blocked['keys'], blocked['count'] = k, k.length
+		res = blocked.to_json
+	else
+		 res = "No key generated"
+	end
+	content_type :json
+	return res
 end
 
 get '/key/release/:id' do
 	#endpoint to release key
+	res = {}
 	if server.release params[:id]
-		res = "released #{params[:id]}"
-	elsif server.free[key]
-		res = "Key already free"
+		res['status'] = true
+		res['message'] = "Released"
+	elsif server.free[params[:id]]
+		res['status'] = false
+		res['message'] = "Key already free"
 	else
-		res = "Key not found"
+		res['status'] = false
+		res['message'] = "Key not found"
 	end
+	content_type :json
+	return res.to_json
 end
 
 get '/key/delete/:id' do
 	#endpoint to delete key
-	res = server.delete(params[:id])? "Key Deleted" : "Key not found"
+	res = {}
+	res['status'] = server.delete(params[:id])
+	res['message'] = res['status'] ? "Key Deleted" : "Key not found"
+	content_type :json
+	return res.to_json
 end
 
 get '/key/alive/:id' do
 	#endpoint to keep the key alive
-	res = server.keep_alive(params[:id])? "Updated" : "Key not found"
+	res = {}
+	res['status'] = server.keep_alive(params[:id])
+	res['message'] = res['status'] ? "Updated" : "Key not found"
+	content_type :json
+	return res.to_json
 end
 
 get '/key/info/:id' do
+	#display key information
+	content_type :json
 	info = server.key_info params[:id]
 	res = info ? info.to_json : "Key not found"
 end
 
+error Sinatra::NotFound do
+	res = {}
+	res['status'] = false
+	res['message'] = "Invalid URL"
+	content_type :json
+	return res.to_json
+end
